263450fc6a727096c498c2c3e930aa04c11a721b
Odim Kingsley <okekings@gmail.com>
2022-11-28 23:56:16 +0000

    Hot fix

--- models/state.py
+++ models/state.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
+
 """Defines the State class."""
+
+
 from models.base_model import BaseModel
 
 
@@ -9,4 +13,5 @@ class State(BaseModel):
         name (str): The name of the state.
     """
 
-    name = ""
+# Attribute
+    name: str = ""

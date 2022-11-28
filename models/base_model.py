89d7dabab5d737a2e21528018fd6d13113f6cac3
David Onwuli <chines_davi@yahoo.com>
2022-11-28 17:17:49 +0100

    God pls

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,7 @@ class BaseModel:
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
+            self.updated_at = datetime.now()
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
@@ -29,10 +30,6 @@ class BaseModel:
     def __str__(self):
         """ returns a string format of the class"""
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
-    
-    def save(self):
-        self.updated_at = datetime.now
-        storage.save()
 
     def to_dict(self):
         my_dict = self.__dict__.copy()
@@ -40,3 +37,7 @@ class BaseModel:
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         return my_dict
+
+    def save(self):
+        self.updated_at = datetime.now
+        storage.save()

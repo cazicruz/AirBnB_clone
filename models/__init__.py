b0d45c4bccc37d4c724500db803520613d972868
Odim Kingsley <okekings@gmail.com>
2022-11-28 23:30:21 +0000

    Init file added

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,11 @@
-from models.engine.file_storage import FileStorage
+#!/usr/bin/python3
+
+"""
+Initializes Module Global Variables (Singleton)
+"""
+
+from .engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()

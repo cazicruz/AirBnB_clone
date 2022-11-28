73d9495f710a0ab96ac2fffabbe82b05d819a71d
David Onwuli <chines_davi@yahoo.com>
2022-11-28 16:40:16 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -20,15 +20,16 @@ class BaseModel:
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
-            self.updated_at = datetime.now()
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self)
             # TODO: add storage
 
     def __str__(self):
         """ returns a string format of the class"""
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+    
     def save(self):
         self.updated_at = datetime.now
         storage.save()
